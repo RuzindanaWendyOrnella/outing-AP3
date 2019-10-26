5a553a51053e19453c2de362c08130be756ffa66
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-10-27 00:10:28 +0200

    added activity for displaying login form

--- app/src/main/java/com/moringaschool/outingapi2/CreateNewAccount.java
+++ app/src/main/java/com/moringaschool/outingapi2/CreateNewAccount.java
@@ -2,13 +2,30 @@ package com.moringaschool.outingapi2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
 
 public class CreateNewAccount extends AppCompatActivity {
+    @BindView(R.id.createUserButton) Button mCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_account);
+            mCreate = (Button) findViewById(R.id.createUserButton);
+
+            mCreate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent=new Intent(CreateNewAccount.this,EscapeActivity.class);
+                    startActivity(intent);
+
+                }
+            });
+        }
     }
-}
+
